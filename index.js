49005ca162e46f688cf2b268deb84b5c867ee18a
JattMannu <manpreet_firewall@yahoo.com.sg>
2017-11-14 23:48:20 +0800

    Added APi

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:admin@ds036577.mlab.com:36577/db_information');
+var conn = mongoose.connection;
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,10 +14,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.get('/add', function (req, res) {
+  new Promise(function (resolve, reject) {
+    const entry = {
+      headers: req.headers,
+      body: req.body
+    }
+    conn.collection('VaultQ').insert(entry);
+    return resolve('Done');
+  }).then(function (data) {
+    res.send(data);
+  }).catch(function (err) {
+    console.log(err);
+  });
+});
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
